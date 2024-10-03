e911ad77b92fb95fa458744263bbdf2be1ae7bf4
Võ Sĩ Mến <vosimen1996@gmail.com>
2024-10-03 14:05:53 +0700

    [U] Update code for release version

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
 dotnet clean -c Release
 dotnet build -c Release
-dotnet publish  -c Release -f ./Installation/PublishApp /p:RuntimeIdentifierOverride=win10-x64
+dotnet publish  -c Release -f net8.0-windows10.0.19041.0 /p:RuntimeIdentifierOverride=win10-x64
 pause
